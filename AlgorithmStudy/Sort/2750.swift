3db5d185c72058c5d408afec8ebc0963c4250867
zaehorang <zaehorang2@naver.com>
2024-07-19 12:39:09 +0900

    add: BOJ_2750 solve

---
+++ AlgorithmStudy/Sort/2750.swift
@@ -0,0 +1,20 @@
+//
+//  2750.swift
+//  AlgorithmStudy
+//
+//  Created by zaehorang on 7/19/24.
+//  https://www.acmicpc.net/problem/2750
+//  수 정렬하기
+
+func _2750() {
+    let n = Int(readLine()!)!
+    var arr: [Int] = []
+    
+    for _ in 0..<n {
+        arr.append(Int(readLine()!)!)
+    }
+    arr.sort()
+    for n in arr {
+        print(n)
+    }
+}

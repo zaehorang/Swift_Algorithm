6323b2f6bcc39acd1b6c07ca344a1e7428e9167d
zaehorang <zaehorang2@naver.com>
2024-07-22 13:01:19 +0900

    add: BOJ_2751 solve

--- AlgorithmStudy/Sort/2751.swift
+++ AlgorithmStudy/Sort/2751.swift
@@ -1,21 +1,19 @@
 //
-//  2750.swift
+//  2751.swift
 //  AlgorithmStudy
 //
-//  Created by zaehorang on 7/19/24.
-//  https://www.acmicpc.net/problem/2750
-//  수 정렬하기
+//  Created by zaehorang on 7/22/24.
+//  https://www.acmicpc.net/problem/2751
+//  수 정렬하기 2
 
-func _2750() {
+func _2751() {
     let n = Int(readLine()!)!
     var arr: [Int] = []
-    
+
     for _ in 0..<n {
         arr.append(Int(readLine()!)!)
     }
-    
     arr.sort()
-    
     for n in arr {
         print(n)
     }

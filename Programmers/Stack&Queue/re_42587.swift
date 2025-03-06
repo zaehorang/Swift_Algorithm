7d9b45aa636ff614d50e7b4fd72b11ea1e5e8f28
zaehorang <jaho001@uos.ac.kr>
2025-03-06 16:50:44 +0900

    add: Programmers_42587 comment

--- Programmers/Stack&Queue/re_42587.swift
+++ Programmers/Stack&Queue/re_42587.swift
@@ -9,6 +9,7 @@ import Foundation
 
 func reSolution_42587(_ priorities:[Int], _ location:Int) -> Int {
     
+    // 처음 위치 정보를 계속 갖고 있어야 한다!
     var arr = priorities.enumerated().map {
         ($0.offset, $0.element)
     }
@@ -16,6 +17,7 @@ func reSolution_42587(_ priorities:[Int], _ location:Int) -> Int {
     while true {
         let elem = arr.removeFirst()
     
+        // 해당 요소가 제일 큰지 비교!
         if arr.contains(where: { elem.1 < $0.1 }) {
             arr.append(elem)
         } else {

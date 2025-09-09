f988bbde0e05761a36ba18475fa9d602c6e58066
zaehorang <jaho001@uos.ac.kr>
2025-09-09 15:48:14 +0900

    add: BOJ_1074 re_ solve

--- AlgorithmStudy/BOJ/Recursion/1074.swift
+++ AlgorithmStudy/BOJ/Recursion/1074.swift
@@ -24,7 +24,7 @@ func _1074() {
 
 func recursion_1074(n: Int, r: Int, c: Int) -> Int {
     if n == 0 { return 0 }
-    let half = 1 << (n-1)
+    let half = 1 << (n-1)  // 2^(n-1)
     if r < half && c < half { return recursion_1074(n: n - 1, r: r, c: c) }
     if r < half && c >= half { return recursion_1074(n: n - 1 , r: r, c: c - half) + half * half }
     if r >= half && c < half { return recursion_1074(n: n - 1 , r: r - half, c: c) + 2 * half * half }
@@ -32,4 +32,22 @@ func recursion_1074(n: Int, r: Int, c: Int) -> Int {
     return recursion_1074(n: n - 1, r: r - half, c: c - half) + 3 * half * half
     
 }
+
+func re_1074() {
+    let nrc = readLine()!.split(separator: " ").map { Int($0)! }
+    let (n, r, c) = (nrc[0], nrc[1], nrc[2])
+    print(re_recursion_1074(n: n, r: r, c: c))
+}
+
+func re_recursion_1074(n: Int, r: Int, c: Int) -> Int {
+    if n == 0 { return 0 }
+    let half = 1 << (n - 1)
+    let areaSize = half * half
+    if r < half && c < half { return re_recursion_1074(n: n - 1, r: r, c: c)}
+    if r < half && c >= half { return areaSize + re_recursion_1074(n: n - 1, r: r, c: c - half)}
+    if r >= half && c < half { return 2 * areaSize + re_recursion_1074(n: n - 1, r: r - half, c: c)}
+    
+    return 3 * areaSize + re_recursion_1074(n: n - 1, r: r - half, c: c - half)
+}
+
 
--- AlgorithmStudy/main.swift
+++ AlgorithmStudy/main.swift
@@ -5,5 +5,5 @@
 //  Created by zaehorang on 4/26/24.
 //
 
-re_1629()
+re_1074()
 

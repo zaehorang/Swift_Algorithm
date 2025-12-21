ff461f72ceffd2e4f89e051bde57ed3b0a83fbbc
zaehorang <jaho001@uos.ac.kr>
2025-12-21 22:15:34 +0900

    add: BOJ_1152 solve

---
+++ AlgorithmStudy/BOJ/_ETC/1152.swift
@@ -0,0 +1,13 @@
+//
+//  1152.swift
+//  AlgorithmStudy
+//
+//  Created by zaehorang on 12/21/25.
+//
+
+import Foundation
+
+func _1152() {
+    let ans = readLine()!.split(separator: " ").count
+    print(ans)
+}

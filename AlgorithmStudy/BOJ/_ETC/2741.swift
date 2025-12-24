e74c597769eba8fb247b9397b664889a12dc8884
zaehorang <jaho001@uos.ac.kr>
2025-12-24 18:57:00 +0900

    add: BOJ_2741 solve

---
+++ AlgorithmStudy/BOJ/_ETC/2741.swift
@@ -0,0 +1,13 @@
+//
+//  2741.swift
+//  AlgorithmStudy
+//
+//  Created by zaehorang on 12/24/25.
+//
+
+import Foundation
+
+func _2741() {
+    let n = Int(readLine()!)!
+    (1...n).forEach { print($0) }
+}
--- AlgorithmStudy/main.swift
+++ AlgorithmStudy/main.swift
@@ -5,4 +5,4 @@
 //  Created by zaehorang on 4/26/24.
 //
 
-_2739()
+_2741()

aec41a46b2084a4819321c7ef2e873d989affdca
zaehorang <jaho001@uos.ac.kr>
2025-12-24 18:46:52 +0900

    add: BOJ_2675 solve

---
+++ AlgorithmStudy/BOJ/_ETC/2675.swift
@@ -0,0 +1,22 @@
+//
+//  2675.swift
+//  AlgorithmStudy
+//
+//  Created by zaehorang on 12/24/25.
+//
+
+import Foundation
+
+func _2675() {
+    let T = Int(readLine()!)!
+    
+    for _ in 0..<T {
+        let command = readLine()!.split(separator: " ")
+        let (n, str) = (Int(command[0])!, String(command[1]))
+        
+        let string = str.map {
+            String(repeating: $0, count: n)
+        }.joined()
+        print(string)
+    }
+}

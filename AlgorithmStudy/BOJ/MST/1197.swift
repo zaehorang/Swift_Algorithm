07852ab96617cc40d3e07f744d22772ef3ad6763
zaehorang <jaho001@uos.ac.kr>
2025-12-31 13:09:10 +0900

    add: BOJ_1197 differently solve

--- AlgorithmStudy/BOJ/MST/1197.swift
+++ AlgorithmStudy/BOJ/MST/1197.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-func _1197() {
+/// Kruskal Algorithm
+func _1197_Kruskal() {
     struct UnionFind {
         private var arr: [Int]
         
@@ -64,4 +65,108 @@ func _1197() {
         if cnt == V - 1 { break }
     }
     print(ans)
+}
+
+
+/// Prim's Algorithm
+func _1197_Prim() {
+    struct Edge {
+        let from: Int
+        let to: Int
+        
+        let cost: Int
+    }
+    
+    struct MinEdgeHeap {
+        private var queue: [Edge] = [Edge(from: 0, to: 0, cost: 0)]
+        private var size = 0
+        
+        var top: Edge { size >= 1 ? queue[1] : queue[0] }
+        var isEmpty: Bool { size == 0 }
+        
+        private let areSorted: (Edge, Edge) -> Bool = { $0.cost < $1.cost }
+        
+        mutating func push(_ edge: Edge) {
+            queue.append(edge)
+            size += 1
+            
+            var index = size
+            
+            while index != 1 {
+                let parentIndex = index / 2
+                
+                if areSorted(queue[parentIndex], queue[index]) { break }
+                
+                queue.swapAt(parentIndex, index)
+                index = parentIndex
+            }
+        }
+        
+        mutating func pop() -> Edge {
+            guard !isEmpty else { return queue[0] }
+            
+            let top = queue[1]
+            queue[1] = queue[size]
+            queue.removeLast()
+            size -= 1
+            
+            var index = 1
+            
+            while 2 * index <= size {
+                let leftChildIndex = index * 2
+                let rightChildIndex = index * 2 + 1
+                
+                var targetIndex = leftChildIndex
+                
+                if rightChildIndex <= size,
+                   areSorted(queue[rightChildIndex], queue[leftChildIndex]) {
+                    targetIndex = rightChildIndex
+                }
+                
+                if areSorted(queue[index], queue[targetIndex]) { break }
+                
+                queue.swapAt(index, targetIndex)
+                index = targetIndex
+            }
+        
+            return top
+        }
+    }
+    
+    let VE = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let (V, E) = (VE[0], VE[1])
+    
+    var graph: [[Edge]] = Array(repeating: [], count: V + 1)
+
+    var check: [Bool] = .init(repeating: false, count: V + 1)
+    var count = 0
+    
+    for _ in 0..<E {
+        let abc = readLine()!.split(separator: " ").compactMap { Int($0) }
+        graph[abc[0]].append(Edge(from: abc[0], to: abc[1], cost: abc[2]))
+        graph[abc[1]].append(Edge(from: abc[1], to: abc[0], cost: abc[2]))
+    }
+    
+    check[1] = true
+    var pq = MinEdgeHeap()
+    
+    for next in graph[1] {
+        pq.push(next)
+    }
+    
+    var totalCount = 0
+    
+    while count < V - 1 {
+        let topEdge = pq.pop()
+        
+        if check[topEdge.to] { continue }
+        
+        totalCount += topEdge.cost
+        count += 1
+        check[topEdge.to] = true
+        
+        graph[topEdge.to].forEach { pq.push($0) }
+    }
+    
+    print(totalCount)
 }

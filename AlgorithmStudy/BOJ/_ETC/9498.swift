00cdfe3e87ab5e8688fb9ce3260f27431c3e46e5
zaehorang <jaho001@uos.ac.kr>
2026-01-22 14:21:45 +0900

    solve: [BOJ] 9498

---
+++ AlgorithmStudy/BOJ/_ETC/9498.swift
@@ -0,0 +1,20 @@
+//
+// 9498.swift
+// Swift_Algorithm
+//
+// Created by zaehorang on 2026/1/22.
+// https://acmicpc.net/problem/9498
+//
+
+import Foundation
+
+func _9498() {
+    // Your solution here
+    switch Int(readLine()!)! {
+    case 90...100: print("A")
+    case 80...89: print("B")
+    case 70...79: print("C")
+    case 60...69: print("D")
+    default: print("F")
+    }
+}

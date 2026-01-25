259859d7827735669917fcf18b081eb7cf1ace14
zaehorang <jaho001@uos.ac.kr>
2026-01-25 18:34:23 +0900

    solve: [BOJ] 10171

---
+++ AlgorithmStudy/BOJ/_ETC/10171.swift
@@ -0,0 +1,20 @@
+//
+// 10171.swift
+// Swift_Algorithm
+//
+// Created by zaehorang on 2026/1/25.
+// https://acmicpc.net/problem/10171
+//
+
+import Foundation
+
+func _10171() {
+    // Your solution here
+    let cat = #"""
+        \    /\
+         )  ( ')
+        (  /  )
+         \(__)|
+        """#
+    print(cat)
+}

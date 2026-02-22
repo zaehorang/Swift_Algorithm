abcbd315ddfbac589f19c7bd3d1b6d4c7ecafac2
zaehorang <jaho001@uos.ac.kr>
2026-02-22 23:01:11 +0900

    solve: [BOJ] 1237

---
+++ AlgorithmStudy/BOJ/1237.swift
@@ -0,0 +1,14 @@
+//
+// 1237.swift
+// Swift_Algorithm
+//
+// Created by zaehorang on 2026/2/22.
+// https://acmicpc.net/problem/1237
+//
+
+import Foundation
+
+func _1237() {
+    // Your solution here
+    print("문제의 정답")
+}

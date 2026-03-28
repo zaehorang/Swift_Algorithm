1b2a886397a9f962e0b0c0f89e55e1654514ec25
zaehorang <jaho001@uos.ac.kr>
2026-03-28 23:25:17 +0900

    solve: [BOJ] 8393

---
+++ AlgorithmStudy/BOJ/8393.swift
@@ -0,0 +1,15 @@
+//
+// 8393.swift
+// Swift_Algorithm
+//
+// Created by zaehorang on 2026/3/28.
+// https://acmicpc.net/problem/8393
+//
+
+import Foundation
+
+func _8393() {
+    // Your solution here
+    let n = Int(readLine()!)!
+    print((1...n).reduce(0, +))
+}

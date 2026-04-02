2fe39e6f0c823390f1ad707e6fc3464433025964
zaehorang <jaho001@uos.ac.kr>
2026-04-02 23:19:38 +0900

    solve: [BOJ] 10718

---
+++ AlgorithmStudy/BOJ/10718.swift
@@ -0,0 +1,15 @@
+//
+// 10718.swift
+// Swift_Algorithm
+//
+// Created by zaehorang on 2026/4/2.
+// https://acmicpc.net/problem/10718
+//
+
+import Foundation
+
+func _10718() {
+    // Your solution here
+    print("강한친구 대한육군")
+    print("강한친구 대한육군")
+}
